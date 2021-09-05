c0637dd8561b2b9c00d439176cbecbea4611dc05
sriramajasti <55838531+sriramajasti@users.noreply.github.com>
2021-09-06 00:01:41 +0530

    Add files via upload
    Java Assignments

---
+++ Java/Activities/Activity12.java
@@ -0,0 +1,22 @@
+package activities;
+
+
+
+interface Add {
+    int add(int a, int b);
+}
+
+public class Activity12 {
+    public static void main(String[] args) {
+
+        // Lambda expression without return keyword.
+        Add ad1 = (a, b) -> (a + b);
+        System.out.println(ad1.add(10, 20));
+
+        // Lambda expression with return keyword.
+        Add ad2 = (int a, int b) -> {
+            return (a + b);
+        };
+        System.out.println(ad2.add(100, 200));
+    }
+}
